4db70c8155e5c2efb33f037b7e6203f3fc7c5ef8
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2023-03-24 09:25:26 -0400

    Update adf_sync.sh

--- script/adf_sync.sh
+++ script/adf_sync.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Clone the prod ADF repo
+git clone https://github.com/calebu/adf_qa_.git
+cd adf_qa
 git clone https://github.com/calebu/adf_cicd.git
-ls
-cd adf_cicd
+rm -rf adf_cicd/.git*
+cp adf_cicd/* .
+git add .
+git commit -m "Pushing latest commit in Prod to lower environments"
+git push --repo https://calebu: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@github.com/calebu/adf_qa_.git
+
 #git remote set-url origin https://github.com/calebu/adf_qa.git
 #git push -u origin master
 #git branch --set-upstream-to https://github.com/calebu/adf_qa.git
