0d4a2c27e0fec9dc2fda4d66d8530f2d96b267d5
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2023-03-24 11:34:07 -0400

    Update adf_sync.sh

--- script/adf_sync.sh
+++ script/adf_sync.sh
@@ -3,6 +3,7 @@
 # Clone the prod ADF repo
 git config --global user.email "calebadeyemi@gmail.com"
 git config --global user.name "Caleb Adeyemi"
+echo '${{ secrets.PAT }}'
 eval `ssh-agent -s`
 chmod 400 ./key_rsa
 ssh-add ./key_rsa
