116b01f745f2921298b474f5f9cf10438d110e3a
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2023-03-24 06:08:29 -0400

    Update adf_sync.sh

--- script/adf_sync.sh
+++ script/adf_sync.sh
@@ -2,6 +2,7 @@
 
 # Clone the prod ADF repo
 git clone https://github.com/calebu/adf_cicd.git
+cd adf_cicd
 git remote set-url origin https://github.com/calebu/adf_qa.git
 git push -u origin master
 #git branch --set-upstream-to https://github.com/calebu/adf_qa.git
