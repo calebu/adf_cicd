3c216b71547baf00dccde6f05caa8bd873426731
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2023-03-24 12:48:52 -0400

    Update adf_sync.sh

--- script/adf_sync.sh
+++ script/adf_sync.sh
@@ -13,7 +13,9 @@ rm -rf adf_cicd/.git*
 cp -r adf_cicd/* .
 rm -rf adf_cicd/
 # Push to lower environment
-chmod 400 ./key_rsa && ssh-add ./key_rsa
+#chmod 400 ./key_rsa && ssh-add ./key_rsa
+
+chmod 400 ./key_rsa && SSH_ASKPASS=/path/to/ssh_give_pass.sh ssh-add ./key_rsa <<< "what"
 git remote set-url origin git@github.com:calebu/adf_qa.git
 git add .
 git commit -m "Pushing latest commit in Prod to lower environments"
