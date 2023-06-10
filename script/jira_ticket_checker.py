81df60a35a2d5fd44411a2deeab331929db7b340
Caleb Adeyemi <calebadeyemi@gmail.com>
2023-06-10 16:09:18 -0400

    Testing workflow

--- script/jira_ticket_checker.py
+++ script/jira_ticket_checker.py
@@ -12,11 +12,11 @@ print(f'Ticket Arg: {pr_comment}')
 
 for i in data:
     print(f'Ticket: {i["ticket"]}, Status: {i["status"]}')
-	if i['ticket'] == pr_comment and i['status'] in valid_statuses:
-		print(0)
-	
-	if i['ticket'] == pr_comment:
-		print(f'Status: {i["status"]}')
-	
+    if i['ticket'] == pr_comment and i['status'] in valid_statuses:
+        print(0)
+    
+    if i['ticket'] == pr_comment:
+        print(f'Status: {i["status"]}')
+    
 f.close()
 print(1)
