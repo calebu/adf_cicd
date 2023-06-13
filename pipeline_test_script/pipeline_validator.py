3c094230b6542b4d632267f1ba8510f164875c9a
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2023-06-13 09:45:08 -0400

    Update pipeline_validator.py

--- pipeline_test_script/pipeline_validator.py
+++ pipeline_test_script/pipeline_validator.py
@@ -3,8 +3,8 @@ prev_commit = sys.argv[1].replace(sys.argv[3], '').split('\n')
 new_commit = sys.argv[2].replace(sys.argv[4], '').split('\n')
 
 print(sys.argv[5])
-vars_ = json.loads(sys.argv[5])
-print(json.dumps(vars_))
+#vars_ = json.loads(sys.argv[5])
+#print(json.dumps(vars_))
 
 s = set(new_commit)
 dropped_pipelines = [x for x in prev_commit if x not in s and x.startswith("/pipeline/Data_Archival")]
