62a2b23ccb2e07c205f0e6baf3958164e7ee8bef
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2023-06-14 20:48:51 -0400

    Update pipeline_validator.py

--- pipeline_test_script/pipeline_validator.py
+++ pipeline_test_script/pipeline_validator.py
@@ -3,7 +3,8 @@ prev_commit = sys.argv[1].replace(sys.argv[3], '').split('\n')
 new_commit = sys.argv[2].replace(sys.argv[4], '').split('\n')
 
 print(sys.argv[5])
-vars_ = json.loads(sys.argv[5].replace('\\"', "'"))
+print(sys.argv[5].replace('\\"', "'"))
+vars_ = json.loads(sys.argv[5])
 print(vars_)
 #print(vars_["pipelines"])
 
