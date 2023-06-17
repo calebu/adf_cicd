dfdde38b3e7668bb7aaf23978d6a2591d3596b33
Caleb Adeyemi <31473599+calebu@users.noreply.github.com>
2023-06-17 17:20:57 -0400

    Add files via upload

--- script/jira_ticket_checker.py
+++ script/jira_ticket_checker.py
@@ -2,13 +2,13 @@ import sys, json
 pr_comment = sys.argv[1].strip()
 valid_statuses = ['In Progress', 'In Development']
 
-f = open('./main/script/jira_tickets.json')
+f = open('./main/jira_validator_script/jira_tickets.json')
 
 data = json.load(f)
 #Find the ticket and status
 
 for i in data:
-    if i['ticket'] == pr_comment and i['status'] in valid_statuses:
+    if i['ticket'] in pr_comment and i['status'] in valid_statuses:
         print("")
         exit()
     
